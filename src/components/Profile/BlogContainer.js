24f7f16481d6266205a07be13846b1e681178aff
Kevin <keb@protonmail.com>
2018-10-29 17:12:40 -0400

    moved Post subcomponent to its own Post.js file

--- src/components/Profile/BlogContainer.js
+++ src/components/Profile/BlogContainer.js
@@ -1,25 +1,6 @@
 import m from 'mithril';
+import { Post } from 'components/Profile/BlogContainer/Post';
 import { EmptyState } from 'components/EmptyState';
-
-/**
- * BlogContainer Post Subcomponent
- */
-const Post = {
-    /**
-     * View Method
-     * @param {Object} attrs View Attributes
-     */
-    view: ({attrs}) => m('.my2', [
-        m('span', [
-            m('.h5.inline.light-subdue', attrs.post.date),
-            m('span.inline.light-subdue.px1', '•'),
-            m('a.h2.inline', {
-                oncreate: m.route.link,
-                href: `/p/${attrs.post.doc_id}`
-            }, attrs.post.title)
-        ])
-    ])
-};
 
 /**
  * Profile BlogContainer Component
